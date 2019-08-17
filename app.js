3e9f00c573f0a0416fae868eeeddb7e2f68fa0e6
Anthony Pogliano <poglianoster@gmail.com>
2019-08-17 12:12:09 -0600

    Removed console logs. Ironed out what to send for res.send() call.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv').config()
+require('dotenv').config()
 const express = require('express')
 const app = express()
 require('./src/db.js')
@@ -6,18 +6,19 @@ const State = require('./src/state.js')
 const port = process.env.PORT || 3001
 
 
-
+// Config
 app.use(express.static('public'))
 app.use(express.json())
 
 
 
+// Routes
 app.get('/test', async (req, res) => {
   try {
     const currentUser = await State.find()
     res.send(currentUser)
   } catch (err) {
-    console.log('There was an error fetching the current user: ', err)
+    console.error('There was an error fetching the current user: ', err)
     res.status(400)
     res.send(err)
   }
@@ -27,15 +28,12 @@ app.put('/test', async (req, res) => {
   // Store new user from request
   // Get 'current user' value from DB and store in var
   // Use updateOne() to pass new user string value 
-  console.log('request body: ', req.body)
   try {
     const newUser = req.body.user
-    console.log('new user: ', newUser)
     const currentUser = await State.find()
-    console.log('current user: ', currentUser)
     await State.updateOne({ user: currentUser[0].user }, { user: newUser })
     res.status(200)
-    res.send()
+    res.send({ user: newUser })
   } catch (err) {
     console.log('There was an error updating the current user: ', err)
     res.status(500)
